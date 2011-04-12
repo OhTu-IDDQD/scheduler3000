23309ccb4f9662f2b8c5d7740db3d3f42b8ae538
Lauri-Henrik Jalonen <ljalonen@dac-37.cs.helsinki.fi>
2011-04-12 16:06:26 +0300

    weekreport csv

--- src/fi/helsinki/cs/scheduler3000/WeekReport.java
+++ src/fi/helsinki/cs/scheduler3000/WeekReport.java
@@ -88,7 +88,63 @@ public class WeekReport extends Report {
         }
         
         public String toCSV() {
-            return "testi";
+
+		if (this.options.containsKey("days")){
+			ArrayList<Weekday.Day> days =
+                                (ArrayList<Day>)this.options.get("days");
+			String[][] reportArray = new String[
+                                Event.VALID_START_TIMES.length+1][days.size()+1]; // +1 for header row
+                        // Nothing in the top left corner
+			reportArray[0][0] = "";
+                        // Add the start times
+			for (int i = 1, j = 0; j < Event.VALID_START_TIMES.length; i++, j++){
+				reportArray[i][0] = Event.VALID_START_TIMES[j];
+			}
+
+			int dayIndex = 1;
+			for (Day day : days){
+                                reportArray[0][dayIndex] = day.toString();
+				ArrayList<Event> events = this.schedule.getSchedule().get(day);
+
+				if (events == null){
+					return null;
+				}
+
+				for (Event event : events){
+					String entry = null;
+
+					if (event.getTitle() != null) {
+                                            entry = event.getTitle();
+					}
+                                        int timeIndex = 0;
+                                        // Find out when the event starts
+                                        for (String startTime : Event.VALID_START_TIMES) {
+                                            timeIndex++;
+                                            if (event.getStartTime().equals(startTime)) {
+                                                reportArray[timeIndex][dayIndex] = entry;
+                                            }
+                                        }
+				}
+				dayIndex++;
+			}
+
+			String weekReport = "";
+                        // Go through the array and append it to a string
+			for (int i = 0; i < reportArray.length; i++){
+				for (int j = 0; j < reportArray[0].length; j++){
+                                        if (reportArray[i][j] != null) {
+                                            weekReport += reportArray[i][j]+";";
+                                        }
+                                        // Fill space with whitespace if there is no event
+                                        else
+                                            weekReport += ";";
+				}
+				weekReport += "\n";
+			}
+
+			return weekReport;
+		}
+		return null;
         }
 
 }
